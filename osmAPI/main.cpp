697729c0019ea87952374c9347fd14935a41f664
MEREL Leo <leomerel@yahoo.fr>
2019-04-10 22:15:52 +0200

    Un peu plus clair

--- osmAPI/main.cpp
+++ osmAPI/main.cpp
@@ -18,7 +18,21 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QUrl url("http://overpass-api.de/api/interpreter?data=%3Cosm-script%20output%3D%22json%22%3E%0A%20%20%3Cquery%20type%3D%22way%22%3E%0A%20%20%20%20%20%20%3Caround%20lat%3D%2248.434420%22%20lon%3D%22-4.640103%22%20radius%3D%221000%22%2F%3E%0A%20%20%20%20%20%20%3Chas-kv%20k%3D%22highway%22%20regv%3D%22tertiary%7Cprimary%7Csecondary%22%2F%3E%0A%20%20%3C%2Fquery%3E%0A%20%20%3Cunion%3E%0A%20%20%20%20%3Citem%2F%3E%0A%20%20%20%20%3Crecurse%20type%3D%22down%22%2F%3E%0A%20%20%3C%2Funion%3E%0A%20%20%3Cprint%2F%3E%0A%3C%2Fosm-script%3E");
+    QString latitude = "48.434420";
+    QString longitude = "-4.640103";
+    QString radius = "1000"; //in meter
+    QString urlString = "http://overpass-api.de/api/interpreter?data=%3Cosm-script%20output%3D%22json"
+            "%22%3E%0A%20%20%3Cquery%20type%3D%22way"
+            "%22%3E%0A%20%20%20%20%20%20%3C"
+            "around%20lat%3D%22"+latitude+
+            "%22%20lon%3D%22"+longitude+
+            "%22%20radius%3D%22"+radius+
+            "%22%2F%3E%0A%20%20%20%20%20%20%3Chas-kv%20k%3D%22highway"
+            //here we set the roads we want (motorway,trunk,primary,secondary,tertiary,unclassified,residential)
+            "%22%20regv%3D%22primary%7Csecondary%7Ctertiary%7C"
+            "%22%2F%3E%0A%20%20%3C%2Fquery%3E%0A%20%20%3Cunion%3E%0A%20%20%20%20%3Citem%2F%3E%0A%20%20%20%20%3"
+            "Crecurse%20type%3D%22down%22%2F%3E%0A%20%20%3C%2Funion%3E%0A%20%20%3Cprint%2F%3E%0A%3C%2Fosm-script%3E";
+    QUrl url(urlString);
     QNetworkRequest request(url);
 
     QNetworkAccessManager *manager = new QNetworkAccessManager();
@@ -41,6 +55,7 @@ int main(int argc, char *argv[])
         QJsonDocument jsonResponse = QJsonDocument::fromJson(strReply.toUtf8());
         QJsonObject jsonObj = jsonResponse.object();
 
+        qDebug() << urlString << "\n";
         qDebug() << jsonObj;
     }
 
