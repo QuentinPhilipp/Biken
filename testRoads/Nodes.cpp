2871f0c3d85fccabde2c061b56423da389fd1c10
Philipp Quentin <quentin.philipp@hotmail.fr>
2019-04-10 16:52:36 +0200

    Ajout commentaire + displayGPSData()

--- testRoads/Nodes.cpp
+++ testRoads/Nodes.cpp
@@ -1,10 +1,13 @@
 #include "Nodes.h"
 
 
+//constructor
 Nodes::Nodes(string id,double lat,double lon) : id{id},latitude{lat},longitude{lon}
 {};
 
 
+
+//getter (read-only)
 string Nodes::getid() const
 {
   return this->id;
--- testRoads/Roads.cpp
+++ testRoads/Roads.cpp
@@ -8,11 +8,12 @@ int main()
 {
   Nodes nds1 = Nodes("33438625",48.3767558,-4.5505677);
   Nodes nds2 = Nodes("33438629",48.3768234,-4.5485148);
-  Nodes nds3 = Nodes("33438632",48.3770156,-4.5474697);
-  std::vector<Nodes> nds{nds1,nds2,nds3};
-  string id = "372871887";
-  Ways w = Ways(id,nds);
+  Nodes nds3 = Nodes("33438632",48.3770156,-4.5474697);     //creating false node
+  std::vector<Nodes> nds{nds1,nds2,nds3};                   //vect of node
+  string id = "372871887";                                  //create an id
+  Ways w = Ways(id,nds);                                    //generating way
 
-  w.displayNode();
+  w.displayNode();        //just display the node Id
+  w.displayGPSData();     //display all the node and the GPS data
   return 0;
 };
