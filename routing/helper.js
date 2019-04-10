883fb859461dbd2917b73341df25b0a8961e2b1a
MEREL Leo <leomerel@yahoo.fr>
2019-04-10 11:50:06 +0200

    Basic routing map

---
+++ routing/helper.js
@@ -0,0 +1,41 @@
+.pragma library
+
+function roundNumber(number, digits)
+{
+    var multiple = Math.pow(10, digits);
+    return Math.round(number * multiple) / multiple;
+}
+
+function formatTime(sec)
+{
+    var value = sec
+    var seconds = value % 60
+    value /= 60
+    value = (value > 1) ? Math.round(value) : 0
+    var minutes = value % 60
+    value /= 60
+    value = (value > 1) ? Math.round(value) : 0
+    var hours = value
+    if (hours > 0) value = hours + "h:"+ minutes + "m"
+    else value = minutes + "min"
+    return value
+}
+
+function formatDistance(meters)
+{
+    var dist = Math.round(meters)
+    if (dist > 1000 ){
+        if (dist > 100000){
+            dist = Math.round(dist / 1000)
+        }
+        else{
+            dist = Math.round(dist / 100)
+            dist = dist / 10
+        }
+        dist = dist + " km"
+    }
+    else{
+        dist = dist + " m"
+    }
+    return dist
+}
