a983cce454276a683f603b5fd20572cfb940d05f
MEREL Leo <leomerel@yahoo.fr>
2019-04-12 12:04:42 +0000

    Optimisation du code

--- osmAPI/Nodes.h
+++ osmAPI/Nodes.h
@@ -9,16 +9,16 @@ using namespace std;
 class Nodes
 {
   public :
-    Nodes(string id,double lat,double lon); //constructor
+    Nodes(uint64_t id,double lat,double lon); //constructor
 
 
     //getter (no setter bc it is read-only)
-    string getId() const;
+    uint64_t getId() const;
     double getLatitude() const;
     double getLongitude() const;
 
   private :
-    string id;          //id of the Nodes
+    uint64_t id;          //id of the Nodes
     double latitude;    //latitude of the node
     double longitude;   //longitude of the node
 };
