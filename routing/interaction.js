bd555204ac1cb1662ec98401db80461274439fb9
BeuzetCharles <charlesbeuzet@outlook.fr>
2019-05-25 18:58:37 +0200

    Now available on all cards

--- routing/interaction.js
+++ routing/interaction.js
@@ -23,8 +23,17 @@ function haveMarkers(){
 	}
 }
 
-haveMarkers() // on récupére alors tout nos marqueurs
+function haveLayer(){
+    for(item in window) {
+        if(item[0]=='t' && item[1]=='i' && item[2]=='l' && item[3]=="e" && item[4]=="_" && item[5]=="l" && item[6]=="a")
+        {
+        return item 
+        }
+            }
+}
 
+haveMarkers() // on récupére alors tout nos marqueurs
+var layer = window[haveLayer()]
 
 for(var i=0 ; i<markers.length ;++i)
 {
@@ -33,6 +42,20 @@ for(var i=0 ; i<markers.length ;++i)
     		console.log(m.getLatLng()); } )								//window[string] recréé l'objet associé à la string
 }
 
+var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, ' +
+    'AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+
+    
+
+var baseLayers = {
+    "Si tu cliques...": Esri_WorldImagery,
+    "Tu vas ...": layer
+    };
+
+var layerControl = L.control.layers(baseLayers);                //avec ce layer --> peut choisir vue satellite ect --> faire pour retourner à la vue de base
+layerControl.addTo(map);
 
 
 
